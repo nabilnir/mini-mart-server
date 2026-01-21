b4f04a091a3bac6d27a3736b9525e48bf6d99198
nabilnir <nirarhan@gmail.com>
2026-01-21 23:41:21 +0600

    cors updated

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+            "https://mini-mart-client-eight.vercel.app",
+            "minimart-auth.firebaseapp.com",
+            "http://localhost:5173",
+            'http://localhost:5177',
+            'http://localhost:3000'
+        ],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nbsql11.8upnco0.mongodb.net/?appName=NBSQL11`;
